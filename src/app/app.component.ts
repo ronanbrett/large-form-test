ced16505dc95cfbea0f852764012503c9f333567
Ronan Brett <ro.brett@gmail.com>
2020-01-09 20:18:14 +0000

    init test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,39 @@
 import { Component } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { FormlyFieldConfig } from '@ngx-formly/core';
+
+function generateField(id: number) {
+
+  if(id % 5 == 0) {
+    return {
+      key: `id-${id}`,
+      type: 'input',
+      templateOptions: {
+        label: 'Input',
+        placeholder: 'Placeholder',
+        description: 'Description',
+        required: true,
+      },
+      expressionProperties: {
+        'templateOptions.disabled': (model: any, formState: any, field: FormlyFieldConfig) => {
+          // access to the main model can be through `this.model` or `formState` or `model
+          return !(model[`id-${id - 2}`] && model[`id-${id - 1}`])
+        },
+      }
+    }; 
+  }
+
+  return {
+    key: `id-${id}`,
+    type: 'input',
+    templateOptions: {
+      label: 'Input',
+      placeholder: 'Placeholder',
+      description: 'Description',
+      required: true,
+    },
+  };
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +41,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  fields: FormlyFieldConfig[];
+  form: FormGroup = new FormGroup({});
   title = 'form-test';
+
+  model: any = {};
+
+  constructor(){
+    const arr = [];
+    for (let index = 0; index < 150; index++) {
+      arr.push(generateField(index)); 
+    }
+
+    this.fields = arr;
+  }
+
+  // fields: FormlyFieldConfig[] = [
+  //   {
+  //     key: 'Input',
+  //     type: 'input',
+  //     templateOptions: {
+  //       label: 'Input',
+  //       placeholder: 'Placeholder',
+  //       description: 'Description',
+  //       required: true,
+  //     },
+  //   },
+  // ];
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule} from "@angular/forms";
+import { FormlyModule } from "@ngx-formly/core"
+import { FormlyMaterialModule } from "@ngx-formly/material"
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    FormlyModule.forRoot(),
+    FormlyMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
